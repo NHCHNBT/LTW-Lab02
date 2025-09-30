a111133589146eb8197166da8aafe09e37d05729
NHCHNBT <nxichthanh5@gmail.com>
2025-10-01 01:13:35 +0700

    Lab02(2)-LTW

---
+++ Lab02(2)-LTW/Controllers/HamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_2__LTW.Controllers
+{
+    public class HamController : Controller
+    {
+        // GET: Ham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab02(2)-LTW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab02_2__LTW.MvcApplication" Language="C#" %>
