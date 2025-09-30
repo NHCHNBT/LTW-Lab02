726401182009b2a52697ec8f0f86ca9da714849c
NHCHNBT <nxichthanh5@gmail.com>
2025-09-30 22:25:12 +0700

    Lab02-LTW

---
+++ Lab02-LTW/Controllers/Layout1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_LTW.Controllers
+{
+    public class Layout1Controller : Controller
+    {
+        // GET: Layout1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab02-LTW/Controllers/Layout2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02_LTW.Controllers
+{
+    public class Layout2Controller : Controller
+    {
+        // GET: Layout2
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
